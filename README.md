b58f84886a9d8805e857b5e568770cfd3623306a
andygock <andy@agock.com>
2018-01-09 00:17:13 +1100

    Improve test script, update README

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ Node JS module for generating guitar chord diagrams in SVG format. Interval name
 
 ![Example 1](http://andygock.github.io/chordy-svg/Cmaj7-2-x35453.svg) ![Example 2](http://andygock.github.io/chordy-svg/C13-1-8x89aa.svg)
 
+A `<defs>` element is written which contains the notes of the chord. This in turn can later be used by the user for audio previews etc.
+
+```xml
+<defs xmlns="http://www.w3.org/2000/svg" id="tonal">
+  <style id="SvgjsStyle1101"/>
+  <notes id="tonal-notes">C3:G3:B3:E4:G4</notes>
+  <semitones id="tonal-semitones">8:15:19:24:27</semitones>
+  <comment id="chord-comment"/>
+</defs>
+```
+
 The [GitHub page](https://github.com/andygock/chordy-svg) may contain more up to date code than the NPM release. Please check GitHub for the most recent updates.
 
 ## Demo
@@ -12,9 +23,17 @@ You can see this library being used at <https://chords.gock.net/>
 
 ## Installation
 
+### Via NPM
+
 Install module:
 
     npm install chordy-svg --save
+
+### Via Git
+
+Clone Git repository
+
+    git clone https://github.com/andygock/chordy-svg
 
 ## Simple example
 
@@ -37,7 +56,7 @@ process.stdout.write(data);
 
 If you've cloned the library from GitHub instead of using NPM, you'll need to replace `require('chordy-svg')` with the correct path, e.g `require('./chordy-svg')`
 
-Create SVG
+Create SVG and write to new file `output.svg`
 
     node test.js > output.svg
 
@@ -58,12 +77,18 @@ The properties for this object are:
 
 - `name` is the name of the chord and will be displayed as a title in the SVG
 - `shape` is the shape of the chord, starting from the lowest / thickest string.
-    - `x`: muted or skipped string.
-    - `0`: open string (open strings not properly tested yet)
-    - `[0-9a-f]`: hexadecimal value refers to fret position. e.g `b` refers to fret position `11`. Only up to fret position `15` (hex `f`) is permitted.
+  - `x`: muted or skipped string.
+  - `0`: open string (open strings not properly tested yet)
+  - `[0-9a-f]`: hexadecimal value refers to fret position. e.g `b` refers to fret position `11`. Only up to fret position `15` (hex `f`) is permitted.
 - `root` is the root string. `0` refers to the lowest frequency / thickest string. On 6 string guitars with EADGBE tuning, `5` refers to the high E string. In the generated diagram, the dot for this position will be coloured red by default.
 
 A second parameters to the `ChordySvg()` may be used to set more custom parameters (documentation not complete yet).
+
+## Mocha testing
+
+Run `npm test` or `mocha`
+
+This will also generate two SVG file which you should check. Remove these files once you've finished with them.
 
 ## Notes
 
--- test/test.js
+++ test/test.js
@@ -1,75 +1,139 @@
 /* eslint prefer-arrow-callback:0, func-names:0, no-console:0 */
 
 const ChordySvg = require('../index');
+const fs = require('fs');
+const DOMParser = require('xmldom').DOMParser;
 
 describe('Generate SVG', function () {
+  const svg = new ChordySvg({
+    name: "Cmaj7",
+    shape: "x35453",
+    root: 2,
+    comment: "Test comment"
+  });
+  const data = svg.svg();
+  const doc = new DOMParser().parseFromString(data);
 
-  it('with chord object', function () {
-    const voicing = {
-      name: "Cmaj7",
-      shape: "x35453",
-      root: 2,
-      comment: ""
-    };
-    const svg = new ChordySvg(voicing);
-    const data = svg.svg();
-    if (!data.startsWith('<svg xmlns=')) {
-      throw new Error('Invalid SVG output');
-    }
+  describe('with valid CMaj7 chord definition', function() {
+
+    it('<svg> ok', function () {
+      if (doc.documentElement.getElementsByTagName('svg').length !== 1) {
+        throw new Error("Missing <svg> tag");
+      }
+    });
+
+    it('<notes> is C3:G3:B3:E4:G4', function () {
+      const notes = doc.documentElement.getElementsByTagName('notes')[0];
+      if (!notes) {
+        throw new Error('Missing <notes>');
+      }
+      if (notes.textContent != "C3:G3:B3:E4:G4") {
+        throw new Error("Notes are incorrect");
+      }
+    });
+
+    it('<semitones> is 8:15:19:24:27', function () {
+      const semitones = doc.documentElement.getElementsByTagName('semitones')[0];
+      if (!semitones) {
+        throw new Error('Missing <semitones>');
+      }
+      if (semitones.textContent != "8:15:19:24:27") {
+        throw new Error("Semitones are incorrect");
+      }
+    });
+
+    it('<comment> is "Test Comment"', function () {
+      const comment = doc.documentElement.getElementsByTagName('comment')[0];
+      if (!comment) {
+        throw new Error('Missing <comment>');
+      }
+      if (comment.textContent != "Test comment") {
+        throw new Error("comment are incorrect");
+      }
+    });
   });
 
   it('fails with invalid char in chord shape', function () {
-    const voicing = {
+    expectError({
       name: "Cmaj7",
       shape: "x3z453",
       root: 2,
       comment: ""
-    };
-    try {
-      const svg = new ChordySvg(voicing);
-      const data = svg.svg();
-    } catch (err) {
-      // error thrown as expected
-      console.log(err.message);
-      return;
-    }
-    throw new Error('Error not thrown');
+    });
   });
 
   it('fails with invalid root position', function () {
-    const voicing = {
+    expectError({
       name: "Cmaj7",
       shape: "x35453",
       root: 9,
       comment: ""
-    };
-    try {
-      const svg = new ChordySvg(voicing);
-      const data = svg.svg();
-    } catch (err) {
-      // error thrown as expected
-      console.log(err.message);
-      return;
-    }
-    throw new Error('Error not thrown');
+    });
   });
 
   it('fails with root on muted string', function () {
-    const voicing = {
+    expectError({
       name: "Cmaj7",
       shape: "x35453",
       root: 1,
       comment: ""
-    };
-    try {
-      const svg = new ChordySvg(voicing);
-      const data = svg.svg();
-    } catch (err) {
-      // error thrown as expected
-      console.log(err.message);
-      return;
-    }
-    throw new Error('Error not thrown');
+    });
   });
 
 });
+
+describe('Write SVGs to files (user to review and check files afterwards)', function () {
+  const chords = [
+    {
+      name: "Cmaj7",
+      shape: "x35453",
+      root: 2,
+      comment: ""
+    },
+    {
+      name: "C13sus4",
+      shape: "8x8aaa",
+      root: 1,
+      comment: "No 5th"
+    }
+  ];
+
+  for (let i = 0; i < chords.length; i++) {
+    const chord = chords[i];
+    const filename = `${chord.name}-${chord.root}-${chord.shape}.svg`;
+    it(`write to file: ${filename}`, function (done) {
+      // write to file
+      saveToFile(chord, filename, done);
+    });
+  }
+
+  after(function() {
+    // console.log("You'll need to clean up the *.svg files after checking them.");
+  });
+});
+
+function saveToFile(voicing, filename, done) {
+  const svg = new ChordySvg(voicing);
+  const data = svg.svg();
+  if (!data.startsWith('<svg xmlns=')) {
+    throw new Error('Invalid SVG data');
+  }
+  fs.writeFile(filename, data, function (err) {
+    if (err) {
+      throw new Error(err);
+    }
+    done();
+  })
+}
+
+function expectError(voicing) {
+  try {
+    const svg = new ChordySvg(voicing);
+    const data = svg.svg();
+  } catch (err) {
+    // error thrown as expected
+    // console.log('Error: ' + err.message);
+    return;
+  }
+  throw new Error('Error not thrown');
+}
